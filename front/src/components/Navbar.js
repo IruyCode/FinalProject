a674e97e5f5640661eff085400482f0e801fdc13
leodantas96 <leonardodantas24@hotmail.com>
2022-01-25 10:08:25 +0000

    Fixed navbar issues

--- front/src/components/Navbar.js
+++ front/src/components/Navbar.js
@@ -4,11 +4,11 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
     return (
         <nav className="navbar">
-            <div className="navbar_logo">
+            <div className="navbar__logo">
                 <h2>MERN Shopping Cart</h2>
             </div>
             
-            <ul className="navbar_links"> 
+            <ul className="navbar__links"> 
                 <li>
                     <Link to="/cart"> 
                         <i className="fas fa-shopping-cart"></i>
@@ -18,10 +18,7 @@ const Navbar = () => {
                 </li>
 
                 <li>
-                    <Link to="/cart"> 
-                        Cart
-                        <span className="cartlogo__badge">0</span>
-                    </Link>
+                    <Link to="/">Shop</Link>
                 </li>
             </ul>
 
