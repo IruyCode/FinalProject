ccc6c64d7312e6c3d57bbe568f45a7669dda5fd5
leodantas96 <leonardodantas24@hotmail.com>
2022-01-25 11:21:57 +0000

    BackDrop and SideDrawer components

--- front/src/App.js
+++ front/src/App.js
@@ -7,12 +7,16 @@ import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen'
 
 // Components
-import Navbar from './components/Navbar'
+import Navbar from './components/Navbar';
+import Backdrop from './components/Backdrop';
+import SideDrawer from './components/SideDrawer'
 
 function App() {
   return (
     <Router>
       <Navbar />
+      <SideDrawer />
+      <Backdrop />
       <main>
         <Routes>
           <Route exact path="/homescreen" component={HomeScreen}/>
---
+++ front/src/components/Backdrop.js
@@ -0,0 +1,12 @@
+import './Backdrop.css';
+
+const Backdrop = () => {
+    return (
+        <div className="backdrop">
+
+        </div>
+    )
+}
+
+export default Backdrop
+
