1e38545170e2108fe659ca92a591e2605ff33223
leodantas96 <leonardodantas24@hotmail.com>
2022-01-27 10:35:27 +0000

    new

--- BackEnd/data/products.js
+++ BackEnd/data/products.js
@@ -4,19 +4,19 @@ const products = [
         imgUrl:
             "https://techprincess.it/wp-content/uploads/2020/07/playstation-5-545x307.jpg",
         description:
-            "Playstation 5 is awesome.",
-        price: 499,
+            "The PS5 console unleashes new gaming possibilities that you never anticipated. Experience lightning fast loading with an ultra-high speed SSD, deeper immersion with support for haptic feedback, adaptive triggers, and 3D Audio*, and an all-new generation of incredible PlayStation games. *3D audio via built-in TV speakers or analog/USB stereo headphones. Set up and latest system software update required.",
+        price: 599,
         countInStock: 15,
     },
 
     {
-        name: "PlayStation 4",
+        name: "NVIDIA GeForce RTX 3080",
         imgUrl:
-            "https://techprincess.it/wp-content/uploads/2020/07/playstation-5-545x307.jpg",
+            "https://www.tejar.pk/media/catalog/product/cache/3/image/9df78eab33525d08d6e5fb8d27136e95/n/v/nvidia_geforce_rtx_3080_founders_edition_-_5tejar_1.jpg",
         description:
-            "Playstation 4 is awesome.",
-        price: 399,
-        countInStock: 15,
+            "The GeForce RTX 3080 is an enthusiast-class graphics card by NVIDIA, launched on September 1st, 2020. Built on the 8 nm process, and based on the GA102 graphics processor, in its GA102-200-KD-A1 variant, the card supports DirectX 12 Ultimate. This ensures that all modern games will run on GeForce RTX 3080.",
+        price: 1200,
+        countInStock: 5,
     },
 
     {
@@ -26,7 +26,7 @@ const products = [
         description:
             "Playstation 3 is awesoma.",
         price: 299,
-        countInStock: 15,
+        countInStock: 5,
     },
 
     {
@@ -36,7 +36,7 @@ const products = [
         description:
             "Playstation 2 is awesome.",
         price: 199,
-        countInStock: 15,
+        countInStock: 5,
     },
 
     {
@@ -46,7 +46,7 @@ const products = [
         description:
             "Playstation 1 is awesome.",
         price: 99,
-        countInStock: 15,
+        countInStock: 5,
     },
 
 ];
