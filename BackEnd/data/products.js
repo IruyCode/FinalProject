efd3e89c4936db7bace5e0c17bddfe85ceaac9ac
leodantas96 <leonardodantas24@hotmail.com>
2022-01-27 10:56:39 +0000

    Products change

--- BackEnd/data/products.js
+++ BackEnd/data/products.js
@@ -20,32 +20,32 @@ const products = [
     },
 
     {
-        name: "PlayStation 3",
+        name: "Samsung Odyssey G9 Gaming Monitor",
         imgUrl:
-            "https://techprincess.it/wp-content/uploads/2020/07/playstation-5-545x307.jpg",
+            "https://pulsogamer.com.py/7644-thickbox_default/monitor-gamer-samsung-49-odyssey-g9-qled-uhd-4k-curvo.jpg",
         description:
-            "Playstation 3 is awesoma.",
-        price: 299,
+            "Gamers need vivid color and smooth motion to get the most out of their play. A screen with a 1000R curve to its 49 inches offering up a bit more of the surroundings only makes it sweeter. You get all of that in the Odyssey G9, which has a native 240Hz display at 5,120-by-1,440-pixel resolution (that's a 32:9 aspect ratio) in a curve. Most curved monitors have a radius (R) of 1800 to 3800...the lower R number of 1000 means it will surround you all the more.",
+        price: 1599,
         countInStock: 5,
     },
 
     {
-        name: "PlayStation 2",
+        name: "Corsair One a200",
         imgUrl:
-            "https://techprincess.it/wp-content/uploads/2020/07/playstation-5-545x307.jpg",
+            "https://www.notebookcheck.com/fileadmin/_processed_/3/4/csm_3_1080.e902ed3a_acae238a1f.jpg",
         description:
-            "Playstation 2 is awesome.",
-        price: 199,
+            "Desktop PCs come in all shapes and prices these days, but few look as cool as this ultra-quiet tower. That's because Corsair's One a200 is liquid-cooled, so the AMD Ryzen 9 5900X inside is kept cold enough to deliver some stellar gaming performance. The dimensions of the sculpted-metal tower itself are ultracompact, one that goes back to 2017, measuring 15 by 6.9 by 7.9 inches—that's one-third the size of a typical mid-tower. The front sports ports galore with two zones of RGB color. If the gamer on your list lacks space but wants the ultimate gaming tower anyway, this is the one to get.",
+        price: 3799,
         countInStock: 5,
     },
 
     {
-        name: "PlayStation 1",
+        name: "Vaonis Stellina Observation Station",
         imgUrl:
-            "https://techprincess.it/wp-content/uploads/2020/07/playstation-5-545x307.jpg",
+            "https://www.axismag.jp/axismag-admin/wp-content/uploads/2021/01/stellina01.png",
         description:
-            "Playstation 1 is awesome.",
-        price: 99,
+            "Hubble would probably argue that telescope technology has come a long way, but the scopes you can usually put on your deck to check the stars don't seem much different than from Galileo's day. But Vaonis has kicked us into smart telescope territory. The company's original scope, called the Stellina, puts a telescope that is part digital camera (with a Sony image sensor) on top of a stable tripod, all in a unit weighing under 25 pounds and measuring 19 by 15 by 4.7 inches. Connect it to a smartphone or tablet and use it to control the view and record or share 6.4-megapixel images of the planets. Multiple users, up to 10, can connect to the Stellina at once.",
+        price: 3999,
         countInStock: 5,
     },
 
